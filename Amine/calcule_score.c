3e92811c78aa6ad0f3f3db72498d23e2199b4c79
Mohamed Amine Zinddine <44125804+MAmineZZ@users.noreply.github.com>
2021-01-30 19:08:41 +0100

    Update calcule_score.c

--- Amine/calcule_score.c
+++ Amine/calcule_score.c
@@ -1,12 +1,29 @@
 //Fonction qui cumule la valeur des tuiles pour calculer le score
 int calcul_score()
 {
-    //on initialise le score par 0
-    j->score=0;
+    int num_vain;
+    j[i].score=0;
     //Le score du joueur qui a encore de tuiles doit etre negatif
-    for(int i=0; i<j->nbr_tuile; i++)
+    for(int tt=0; tt<j[i].nbr_tuile; tt++)
     {
-        j->score=j->score - j->tuiles[i].chiffre;
+        j[i].score= j[i].score - j[i].tuiles[tt].chiffre;
     }
-    return j->score;
+    //score du winner
+     if(j[i].score==0)
+     {
+         num_vain=j[i].numero;
+
+          for(int role=0; role<4; role++)
+            {
+                if(num_vain != j[role].numero)
+                {
+                    j[num_vain-1].score = j[num_vain-1].score +-1*j[role].score;
+                }
+            }
+            return j[num_vain-1].score;
+     }
+     else if(j[i].score<0)
+     {
+         return j[i].score;
+     }
 }
