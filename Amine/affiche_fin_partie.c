aa1b6a287a200c94a735843a5fccb4916481b52d
Mohamed Amine Zinddine <44125804+MAmineZZ@users.noreply.github.com>
2021-02-01 18:07:50 +0100

    Update affiche_fin_partie.c

--- Amine/affiche_fin_partie.c
+++ Amine/affiche_fin_partie.c
@@ -19,7 +19,7 @@ void affiche_fin_partie()
     SDL_Rect dest_rect_rep;
 
     //Tester le numero du joueur actuel pour savoir le vainqueur
-    j[i].numero=1;
+    //j[i].numero=1;
     if(j[i].numero==1)
     SDL_CreationImage(renderer, image, texture, dest_rect, "1wins.png");
 
