92ef881591a66b953af609cc1f02900ed9faa3f2
Mohamed Amine Zinddine <44125804+MAmineZZ@users.noreply.github.com>
2021-02-04 15:36:28 +0100

    Update calcule_score.c

--- Amine/calcule_score.c
+++ Amine/calcule_score.c
@@ -1,29 +1,28 @@
-//Fonction qui cumule la valeur des tuiles pour calculer le score
+int role_actuel=1;
 int calcul_score()
 {
     int num_vain;
-    j[i].score=0;
+    j[role_actuel].score=0;
     //Le score du joueur qui a encore de tuiles doit etre negatif
-    for(int tt=0; tt<j[i].nbr_tuile; tt++)
+    for(int tt=0; tt<j[role_actuel].nbr_tuile; tt++)
     {
-        j[i].score= j[i].score - j[i].tuiles[tt].chiffre;
+        j[role_actuel].score= j[role_actuel].score - j[role_actuel].tuiles[tt].chiffre;
     }
+    if(j[role_actuel].score<0)
+        {
+            return j[role_actuel].score;
+        }
     //score du winner
-     if(j[i].score==0)
+     else if(j[role_actuel].score==0)
      {
-         num_vain=j[i].numero;
+         num_vain=j[role_actuel].numero;
 
-          for(int role=0; role<4; role++)
+          for(int i=0; i<nbr_joueurs; i++)
             {
-                if(num_vain != j[role].numero)
+                if(num_vain != j[i].numero)
                 {
-                    j[num_vain-1].score = j[num_vain-1].score +-1*j[role].score;
+                    j[num_vain-1].score = j[num_vain-1].score +-1*j[i].score;
                 }
-            }
-            return j[num_vain-1].score;
-     }
-     else if(j[i].score<0)
-     {
-         return j[i].score;
+            }return j[num_vain-1].score;
      }
 }
